3146ff9cb905aa2dd3b6a9baea01484da48b722a
Dan Korzh <lienglex@gmail.com>
2020-11-14 17:50:32 +0200

    import general styles to main component

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux'
 import store from './Store/index'
+import './styles/general.scss'
 
 import FirstForm from './Components/firstForm/FirstForm'
 
