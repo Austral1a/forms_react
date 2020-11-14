d0fa7b80036b7574d270b09146472c3ec27eea6c
Dan Korzh <lienglex@gmail.com>
2020-11-14 21:39:44 +0200

    add: subscriptions object

---
+++ src/helpers/subscriptions.js
@@ -0,0 +1,14 @@
+export const subscriptions = {
+    us: {
+        free: '0$',
+        basic: '20$',
+        premium: '60$'
+    },
+    ua: {
+        free: '0₴',
+        basic: `${20 * 28}₴`,
+        premium: `${60 * 28}₴`
+    }
+};
+
+export default subscriptions;
