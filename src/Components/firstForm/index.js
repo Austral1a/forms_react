688a10743b0a7c552d893b596d71d2badd4139c9
Dan Korzh <lienglex@gmail.com>
2020-11-14 17:12:24 +0200

    add: additional imports for first form

--- src/Components/firstForm/index.js
+++ src/Components/firstForm/index.js
@@ -2,4 +2,21 @@ import Form from '../common/Form'
 import Input from '../common/Input'
 import Button from '../common/Button'
 import {phoneMask} from '../../helpers/masks'
-export {Form, Input, Button, phoneMask}
+import {validationMessages} from '../../helpers/validationMessages'
+import {
+    emailValidation,
+    firstNameValidation,
+    lastNameValidation,
+    phoneValidation
+} from '../../Store/actions/common/validationErrors'
+
+export {
+    Form,
+    Input,
+    Button,
+    phoneMask,
+    validationMessages,
+    emailValidation,
+    firstNameValidation,
+    lastNameValidation,
+    phoneValidation}
