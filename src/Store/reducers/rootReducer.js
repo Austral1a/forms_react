c0bea70720ba5d7f2a7fedec39af806b21db9c97
Dan Korzh <lienglex@gmail.com>
2020-11-14 14:39:06 +0200

    add: rootReducer

---
+++ src/Store/reducers/rootReducer.js
@@ -0,0 +1,8 @@
+import {combineReducers} from 'redux'
+import formValidationReducer from './FormValidationReducer'
+
+const rootReducer = combineReducers({
+      formValidationReducer
+})
+
+export default rootReducer
