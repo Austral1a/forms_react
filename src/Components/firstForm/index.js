a2cf8a618d5949e8450dfc8db0c100b6f0192faf
Dan Korzh <lienglex@gmail.com>
2020-11-14 17:07:59 +0200

    add: first form imports

---
+++ src/Components/firstForm/index.js
@@ -0,0 +1,5 @@
+import Form from '../common/Form'
+import Input from '../common/Input'
+import Button from '../common/Button'
+import {phoneMask} from '../../helpers/masks'
+export {Form, Input, Button, phoneMask}
