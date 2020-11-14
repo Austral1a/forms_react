0618ddeb022ac5b53aeb70877a38e2eec0396cff
Dan Korzh <lienglex@gmail.com>
2020-11-14 20:50:40 +0200

    add: imports for SecondForm component

---
+++ src/Components/secondForm/index.js
@@ -0,0 +1,5 @@
+import Input from '../common/Input'
+import Select from '../common/Select'
+import Option from '../common/Option'
+import Form from '../common/Form'
+export {Input, Select, Option, Form}
