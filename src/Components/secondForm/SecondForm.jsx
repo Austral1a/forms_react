144ec081c161dd941cacc281c0897e7279979a57
Dan Korzh <lienglex@gmail.com>
2020-11-15 12:12:49 +0200

    add: Select handlers for SecondForm

--- src/Components/secondForm/SecondForm.jsx
+++ src/Components/secondForm/SecondForm.jsx
@@ -1,16 +1,34 @@
-import React from 'react'
+import React,{useState} from 'react'
 import {Select, Option, Form, Input, Card} from './index'
 
 import './SecondForm.scss'
 
 function SecondForm() {
+
+    const [country, setCountry] = useState('us')
+    const [plan, setPlan] = useState('free')
+
+    const handleCountry = (e) => {
+        setCountry(e.target.value)
+    }
+
+    const handlePlan = (e) => {
+        setPlan(e.target.value)
+    }
+
     return(
         <Form className='form form-second'>
-            <Select defaultValue='us'>
+            <Select
+                defaultValue='us'
+                onChange={handleCountry}
+            >
                 <Option value='us' text='United States' />
                 <Option value='ua' text='Ukraine' />
             </Select>
-            <Select defaultValue='free'>
+            <Select
+                defaultValue='free'
+                onChange={handlePlan}
+            >
                 <Option value='free' text='Free' />
                 <Option value='basic' text='Basic' />
                 <Option value='premium' text='Premium' />
