366d5094d5b9e9062957122e5143511482a07655
Dan Korzh <lienglex@gmail.com>
2020-11-15 12:11:13 +0200

    add: import getPrice helper for SecondForm

--- src/Components/secondForm/index.js
+++ src/Components/secondForm/index.js
@@ -3,4 +3,6 @@ import Select from '../common/Select'
 import Option from '../common/Option'
 import Form from '../common/Form'
 import Card from '../common/Card'
-export {Input, Select, Option, Form, Card}
+import subscriptions from '../../helpers/subscriptions'
+import getPrice from '../../helpers/getPrice'
+export {Input, Select, Option, Form, Card, subscriptions, getPrice}
