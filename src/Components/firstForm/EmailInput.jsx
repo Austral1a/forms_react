93cc2290b79affec484bde66e7f0161eaf88341c
Dan Korzh <lienglex@gmail.com>
2020-11-15 23:13:29 +0200

    add: EmailInput as standalone component

---
+++ src/Components/firstForm/EmailInput.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import {
+    emailChange,
+    emailSvg,
+    emailValidation,
+    Input,
+    validationMessages as msg
+} from './index'
+
+
+const mapStateToProps = (state) => ({
+    isEmailValid: state.formValidationReducer.isEmailValid,
+    email: state.firstFormReducer.email,
+})
+
+const mapDispatchToProps = (dispatch) => ({
+    emailValidation: (text) => {
+        dispatch(emailValidation(text))
+    },
+    emailChange: (char) => {
+        dispatch(emailChange(char))
+    }
+})
+
+function EmailInput({
+    email,
+    isEmailValid,
+    emailValidation,
+    emailChange
+}) {
+    return(
+        <Input
+            type='text'
+            placeholder='Email'
+            onBlur={() => emailValidation(email)}
+            onChange={e => emailChange(e.target.value)}
+            value={email}
+            hasError={!isEmailValid}
+            errorMessage={msg['email']}
+            icon={emailSvg}
+        />
+    )
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(EmailInput)
