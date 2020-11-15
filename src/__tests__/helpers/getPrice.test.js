7258f301bb4565f0917aab3a582b87dc2d1e9582
Dan Korzh <lienglex@gmail.com>
2020-11-15 22:41:15 +0200

    test: getPrice

---
+++ src/__tests__/helpers/getPrice.test.js
@@ -0,0 +1,41 @@
+import {subscriptions as sub} from '../../helpers/subscriptions'
+import getPrice from '../../helpers/getPrice'
+
+describe('getPrice function must return appropriate subscription', () => {
+    const plans = {
+        free: 'free',
+        basic: 'basic',
+        premium: 'premium'
+    }
+    describe('country -> us', () => {
+        const country = 'us'
+
+        it('plan -> free', () => {
+            expect(getPrice(sub, country, plans.free)).toBe('0$')
+        })
+
+        it('plan -> basic', () => {
+            expect(getPrice(sub, country, plans.basic)).toBe('20$')
+        })
+
+        it('plan -> premium', () => {
+            expect(getPrice(sub, country, plans.premium)).toBe('60$')
+        })
+    })
+
+    describe('country -> ua', () => {
+        const country = 'ua'
+
+        it('plan -> free', () => {
+            expect(getPrice(sub, country, plans.free)).toBe('0₴')
+        })
+
+        it('plan -> basic', () => {
+            expect(getPrice(sub, country, plans.basic)).toBe('560₴')
+        })
+
+        it('plan -> premium', () => {
+            expect(getPrice(sub, country, plans.premium)).toBe('1680₴')
+        })
+    })
+})
