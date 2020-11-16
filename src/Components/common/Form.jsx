68413f2f41f33b10f2e46938b05c7a86e1fc691a
Austral1a <lienglex@gmail.com>
2020-11-16 16:59:01 +0200

    check for classname prop presence

--- src/Components/common/Form.jsx
+++ src/Components/common/Form.jsx
@@ -9,7 +9,7 @@ function Form({
               }) {
     return(
         <form
-            className={className}
+            className={className ? className : ''}
             action={action}
             onSubmit={onSubmit}
         >
