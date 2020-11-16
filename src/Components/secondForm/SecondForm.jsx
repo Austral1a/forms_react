de95fc3cb677494303c72cbf0882a53ac357ad14
Austral1a <lienglex@gmail.com>
2020-11-16 15:34:12 +0200

    SecondForm clean-up

--- src/Components/secondForm/SecondForm.jsx
+++ src/Components/secondForm/SecondForm.jsx
@@ -1,75 +1,24 @@
-import React,{useState} from 'react'
-import {Select, Option, Form, Input, Card, subscriptions, getPrice, countryChange, planChange} from './index'
-import Button from '../common/Button'
-import {connect} from 'react-redux'
-
-
+import React from 'react'
+import {
+    Form,
+    CountrySelect,
+    PlanSelect,
+    PriceCard,
+    ControlPanel} from './index'
 import './SecondForm.scss'
-import {Link} from 'react-router-dom'
 
-const mapStateToProps = (state) => ({
-    country: state.secondFormReducer.country,
-    plan: state.secondFormReducer.plan
-})
-
-const mapDispatchToProps = (dispatch) => ({
-    countryChange: (country) => {
-        dispatch(countryChange(country))
-    },
-    planChange: (plan) => {
-        dispatch(planChange(plan))
-    }
-})
-
-function SecondForm({
-    country,
-    countryChange,
-    plan,
-    planChange
-}) {
-
-    // const [country, setCountry] = useState('us')
-    // const [plan, setPlan] = useState('free')
-
-    const handleCountry = (e) => {
-        countryChange(e.target.value)
-    }
-
-    const handlePlan = (e) => {
-        planChange(e.target.value)
-    }
+function SecondForm() {
 
     return(
         <Form
             onSubmit={(e) => e.preventDefault()}
             className='form form-second'>
-            <Select
-                defaultValue={country}
-                onChange={handleCountry}
-            >
-                <Option value='us' text='United States' />
-                <Option value='ua' text='Ukraine' />
-            </Select>
-            <Select
-                defaultValue={plan}
-                onChange={handlePlan}
-            >
-                <Option value='free' text='Free' />
-                <Option value='basic' text='Basic' />
-                <Option value='premium' text='Premium' />
-            </Select>
-            <Card>
-                {getPrice(subscriptions, country, plan)} per month
-            </Card>
-            <div className='form-second__control'>
-                <Button text={<Link to='/first-form'>Back</Link>} />
-                <Button text='Submit' />
-            </div>
+            <CountrySelect />
+            <PlanSelect />
+            <PriceCard />
+            <ControlPanel />
         </Form>
     )
 }
 
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps)
-(SecondForm)
+export default SecondForm
