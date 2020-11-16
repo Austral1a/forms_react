59cdc0235fc40e7658b12104233c082f14e17d06
Austral1a <lienglex@gmail.com>
2020-11-16 15:30:32 +0200

    add: plan selector as standlone PlanSelect component

---
+++ src/Components/secondForm/PlanSelect.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import {Option, planChange, Select} from './index'
+
+
+const mapStateToProps = (state) => ({
+    plan: state.secondFormReducer.plan
+})
+
+const mapDispatchToProps = (dispatch) => ({
+    planChange: (plan) => {
+        dispatch(planChange(plan))
+    }
+})
+
+function PlanSelect({
+    plan,
+    planChange
+}) {
+
+    const handlePlan = (e) => {
+        planChange(e.target.value)
+    }
+
+    return(
+        <Select
+            defaultValue={plan}
+            onChange={handlePlan}
+        >
+            <Option value='free' text='Free' />
+            <Option value='basic' text='Basic' />
+            <Option value='premium' text='Premium' />
+        </Select>
+    )
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(PlanSelect)
