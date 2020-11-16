98d883cc748205e0725d58687ac4087503e9e64a
Austral1a <lienglex@gmail.com>
2020-11-16 16:37:32 +0200

    test: Card component

---
+++ src/Components/__tests__/Card.test.js
@@ -0,0 +1,33 @@
+import Card from '../common/Card'
+import {shallow} from 'enzyme'
+describe('Card Component' ,() => {
+    it("must set style", () => {
+        const bgColor = '#fff'
+        const wrapper = shallow(<Card style={{backgroundColor: bgColor}} />)
+
+        expect(wrapper.find('.card').prop('style').backgroundColor).toBe(bgColor)
+    })
+
+    it("if className prop exists must set additional className", () => {
+        const className = 'card-first'
+        const wrapper = shallow(<Card className={className} />)
+
+        expect(wrapper.find('.card').prop('className')).toBe(`card ${className}`)
+    })
+
+    it('if className prop is not exists must set additional className', () => {
+        const wrapper = shallow(<Card />)
+
+        expect(wrapper.find('.card').prop('className')).toBe('card')
+        expect(wrapper.find('.card').prop('className')).not.toBe('card ')
+    })
+
+    it("must render children", () => {
+        const text = 'child'
+        const wrapper = shallow(<Card><p>{text}</p></Card>)
+
+        expect(wrapper.find('p')).toHaveLength(1)
+        expect(wrapper.find('p').text()).toBe(text)
+        expect(wrapper.find('p').text()).not.toBe(text + ' ')
+    })
+})
