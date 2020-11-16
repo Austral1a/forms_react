5aa13a3203d8e27245186b55e0ee56ec8ad1e8fe
Austral1a <lienglex@gmail.com>
2020-11-16 14:41:57 +0200

    extracted onClick func into standalone func; set isSubmitted value to false, after render the FirstForm

--- src/Components/firstForm/FirstForm.jsx
+++ src/Components/firstForm/FirstForm.jsx
@@ -1,4 +1,4 @@
-import React, {useMemo} from 'react'
+import React, {useMemo, useEffect} from 'react'
 import {
     Form,
     Button,
@@ -15,6 +15,10 @@ import PhoneInput from './PhoneInput'
 
 const mapStateToProps = (state) => ({
     isSubmitted: state.firstFormReducer.isSubmitted,
+    isFirstNameValid: state.formValidationReducer.isFirstNameValid,
+    isLastNameValid: state.formValidationReducer.isLastNameValid,
+    isEmailValid: state.formValidationReducer.isEmailValid,
+    isPhoneValid: state.formValidationReducer.isPhoneValid,
 })
 
 const mapDispatchToProps = (dispatch) => ({
@@ -35,6 +39,14 @@ function FirstForm({
             anyError(isFirstNameValid, isLastNameValid, isEmailValid, isPhoneValid),
         [isFirstNameValid, isLastNameValid, isEmailValid, isPhoneValid])
 
+    useEffect(() => {
+        // if user is have returned back to the prev form
+        // just to be on the safe side, set isSubmitted for FirstForm - false
+        // anyway, after click on the next button, isSubmitted will compute again
+        firstFormHandler(false)
+    }, [])
+
+    const handleClick = () => firstFormHandler(!anyErrorMemo)
 
     return(
         <Form
@@ -47,7 +59,7 @@ function FirstForm({
             <EmailInput />
             <PhoneInput />
                 <Button
-                    onClick={() => firstFormHandler(!anyErrorMemo)}
+                    onClick={handleClick}
                     disabled={anyErrorMemo}
                     text={<Link to='/second-form'>Next</Link>}
                 />
