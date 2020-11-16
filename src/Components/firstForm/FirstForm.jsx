020ed791aac2f3d21e63ad04b16d70b497cfc81a
Austral1a <lienglex@gmail.com>
2020-11-16 14:57:42 +0200

    clean-up imports; added NextBtn as component

--- src/Components/firstForm/FirstForm.jsx
+++ src/Components/firstForm/FirstForm.jsx
@@ -1,52 +1,14 @@
-import React, {useMemo, useEffect} from 'react'
+import React from 'react'
 import {
     Form,
-    Button,
-    anyError,
-    firstFormHandler,
-    Link,
+    FirstNameInput,
+    LastNameInput,
+    EmailInput,
+    PhoneInput,
+    NextBtn
 } from './index'
 import './FirstForm.scss'
-import {connect} from 'react-redux'
-import FirstNameInput from './FirstNameInput'
-import LastNameInput from './LastNameInput'
-import EmailInput from './EmailInput'
-import PhoneInput from './PhoneInput'
-
-const mapStateToProps = (state) => ({
-    isSubmitted: state.firstFormReducer.isSubmitted,
-    isFirstNameValid: state.formValidationReducer.isFirstNameValid,
-    isLastNameValid: state.formValidationReducer.isLastNameValid,
-    isEmailValid: state.formValidationReducer.isEmailValid,
-    isPhoneValid: state.formValidationReducer.isPhoneValid,
-})
-
-const mapDispatchToProps = (dispatch) => ({
-    firstFormHandler: (noError) => {
-        dispatch(firstFormHandler(noError))
-    }
-})
-
-function FirstForm({
-    isFirstNameValid,
-    isLastNameValid,
-    isEmailValid,
-    isPhoneValid,
-    firstFormHandler,
-}) {
-
-    const anyErrorMemo = useMemo(() =>
-            anyError(isFirstNameValid, isLastNameValid, isEmailValid, isPhoneValid),
-        [isFirstNameValid, isLastNameValid, isEmailValid, isPhoneValid])
-
-    useEffect(() => {
-        // if user is have returned back to the prev form
-        // just to be on the safe side, set isSubmitted for FirstForm - false
-        // anyway, after click on the next button, isSubmitted will compute again
-        firstFormHandler(false)
-    }, [])
-
-    const handleClick = () => firstFormHandler(!anyErrorMemo)
+function FirstForm() {
 
     return(
         <Form
@@ -58,16 +20,9 @@ function FirstForm({
             <LastNameInput />
             <EmailInput />
             <PhoneInput />
-                <Button
-                    onClick={handleClick}
-                    disabled={anyErrorMemo}
-                    text={<Link to='/second-form'>Next</Link>}
-                />
+            <NextBtn />
         </Form>
     )
 }
 
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(FirstForm)
+export default FirstForm
