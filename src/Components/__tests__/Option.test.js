478aaac66901caca45e120be86093509c2b48348
Austral1a <lienglex@gmail.com>
2020-11-16 18:16:11 +0200

    test: Option component

---
+++ src/Components/__tests__/Option.test.js
@@ -0,0 +1,17 @@
+import {shallow} from 'enzyme'
+import Option from '../common/Option'
+
+describe('Option component', () => {
+    it("must render option's inner text", () => {
+        const text = 'this is options'
+        const wrapper = shallow(<Option text={text} />)
+        expect(wrapper.find('.select__option').text()).toBe(text)
+        expect(wrapper.find('.select__option').text()).not.toBe(" " + text)
+    })
+
+    it("option must have value attribute", () => {
+        const value = 'option1'
+        const wrapper = shallow(<Option value={value} />)
+        expect(wrapper.find('select__option')).toHaveAttribute('value', value)
+    })
+})
