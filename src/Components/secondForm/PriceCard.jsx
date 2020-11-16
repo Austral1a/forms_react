ceefaab0b29d7d1e2768d6bb36baf9a0cdb9e186
Austral1a <lienglex@gmail.com>
2020-11-16 15:32:04 +0200

    add: subscription price as standalone PriceCard component

---
+++ src/Components/secondForm/PriceCard.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import {Card, getPrice, subscriptions} from './index'
+
+const mapStateToProps = (state) => ({
+    country: state.secondFormReducer.country,
+    plan: state.secondFormReducer.plan
+})
+
+function PriceCard({
+    country,
+    plan
+}) {
+    return(
+        <Card>
+            {getPrice(subscriptions, country, plan)} per month
+        </Card>
+
+    )
+}
+
+export default connect(
+    mapStateToProps
+)(PriceCard)
