88295af820115812a16d56976947fe2f0686e91c
Austral1a <lienglex@gmail.com>
2020-11-17 14:16:59 +0200

    test: Select component

---
+++ src/Components/__tests__/Select.test.js
@@ -0,0 +1,43 @@
+import Select from '../common/Select/Select'
+import {shallow} from 'enzyme'
+
+describe("Select component", () => {
+    it("must render children", () => {
+        const children = <option value="1">1</option>
+        const wrapper = shallow(<Select>{children}</Select>)
+
+        expect(wrapper.find('select').contains(children)).toBeTruthy()
+    })
+
+    describe("defaultValue prop", () => {
+        it("must set 'defaultValue' prop", () => {
+            const defValue = 'us'
+            const wrapper = shallow(<Select defaultValue={defValue} />)
+
+            expect(wrapper.find('select').prop('defaultValue')).toBe(defValue)
+        })
+
+        it("must not set 'defaultValue' prop", () => {
+            const wrapper = shallow(<Select />)
+
+            expect(wrapper.find('select').prop('defaultValue')).toBe(null)
+        })
+    })
+
+    it("must accurate manage 'onChange' prop", () => {
+        const newValue = 'ua'
+        const initValue = 'us'
+        const state = {
+            value: initValue
+        }
+
+        const onChange = () => {
+            state['value'] = newValue
+        }
+
+        const wrapper = shallow(<Select onChange={onChange} />)
+        expect(state.value).toBe(initValue)
+        wrapper.find('select').simulate('change')
+        expect(state.value).toBe(newValue)
+    })
+})
