cf476c71027a05f6b8cd2616153230be437b471c
Austral1a <lienglex@gmail.com>
2020-11-17 11:40:29 +0200

    deleted validation errors action and its dependencies

--- src/Components/firstForm/index.js
+++ src/Components/firstForm/index.js
@@ -3,12 +3,6 @@ import Input from '../common/Input'
 import Button from '../common/Button'
 import {phoneMask} from '../../helpers/masks'
 import {validationMessages} from '../../helpers/validationMessages'
-import {
-    emailValidation,
-    firstNameValidation,
-    lastNameValidation,
-    phoneValidation
-} from '../../Store/actions/common/validationErrors'
 import anyError from '../../helpers/isThereValidationError'
 import {Link} from 'react-router-dom'
 import userSvg from '../../assets/user.svg'
@@ -30,10 +24,6 @@ export {
     Button,
     phoneMask,
     validationMessages,
-    emailValidation,
-    firstNameValidation,
-    lastNameValidation,
-    phoneValidation,
     anyError,
     Link,
     userSvg,
