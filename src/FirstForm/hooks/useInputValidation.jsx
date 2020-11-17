39d2941d3303aa173e33bb5ac1d9014110053049
Dan Korzh <lienglex@gmail.com>
2020-11-17 22:46:15 +0200

    return memoized val rather then state

--- src/FirstForm/hooks/useInputValidation.jsx
+++ src/FirstForm/hooks/useInputValidation.jsx
@@ -3,21 +3,11 @@ import isInputValid from '../../helpers/isInputValid'
 import patterns from '../../helpers/patterns'
 
 function useInputValidation(patternName, inputName) {
-    const [isValid, setIsValid] = useState(false)
-
-    const isValidMemo = useMemo(
+    return useMemo(
         () => {
             return isInputValid(patterns[patternName], inputName)
         }, [inputName]
     )
-    useEffect(() => {
-        if(isValidMemo) {
-            setIsValid(true)
-        } else {
-            setIsValid(false)
-        }
-    })
-    return isValid
 }
 
 export default useInputValidation
