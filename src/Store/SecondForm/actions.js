fc0f4d379c7737655fb48530fc4cbfae74dde658
Austral1a <lienglex@gmail.com>
2020-11-18 16:39:59 +0200

    add: payload obj

--- src/Store/SecondForm/actions.js
+++ src/Store/SecondForm/actions.js
@@ -2,10 +2,14 @@ import {COUNTRY_SELECT_CHANGE, PLAN_SELECT_CHANGE} from './action-types'
 
 export const countryChange = (country) => ({
     type: COUNTRY_SELECT_CHANGE,
-    country
+    payload: {
+        country
+    }
 })
 
 export const planChange = (plan) => ({
     type: PLAN_SELECT_CHANGE,
-    plan
+    payload: {
+        plan
+    }
 })
