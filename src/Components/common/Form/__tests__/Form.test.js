b786bfe8a5ed5eb7b044aa97be2a1b2c175a48eb
Austral1a <lienglex@gmail.com>
2020-11-18 15:28:35 +0200

    upadted Form test

--- src/Components/common/Form/__tests__/Form.test.js
+++ src/Components/common/Form/__tests__/Form.test.js
@@ -10,17 +10,18 @@ describe('Form component', () => {
         expect(wrapper.find('p').text()).toBe(text)
     })
 
+    const defClassName = 'form'
     it("if prop 'className' exists, form should have that class", () => {
         const wrapper = shallow(<Form className='form-first' />)
 
 
-        expect(wrapper.find('form').prop('className')).toBe('form-first')
+        expect(wrapper.find('form').prop('className')).toBe(`${defClassName} form-first`)
     })
 
-    it("if prop 'className' is not exists, form should not have that class", () => {
+    it("if prop 'className' is not exists, form should have default class", () => {
         const wrapper = shallow(<Form />)
 
-        expect(wrapper.find('form').prop('className')).toBe('')
+        expect(wrapper.find('form').prop('className')).toBe(defClassName)
     })
 
     it("must correctly handle 'onSubmit' prop", () => {
