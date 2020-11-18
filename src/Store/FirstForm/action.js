e410bc12447a26f830f763369d84ac8108c242b6
Austral1a <lienglex@gmail.com>
2020-11-18 16:14:55 +0200

    renamed: saveValues -> action; saveValuesReducer -> reducer

--- src/Store/FirstForm/action.js
+++ src/Store/FirstForm/action.js
@@ -2,10 +2,12 @@ import {SAVE_VALUES_IN_STORE} from './action-types'
 
 const saveValues = (firstName, lastName, email, phone) => ({
     type: SAVE_VALUES_IN_STORE,
-    firstName,
-    lastName,
-    email,
-    phone
+    payload: {
+        firstName,
+        lastName,
+        email,
+        phone
+    }
 })
 
 export default saveValues
