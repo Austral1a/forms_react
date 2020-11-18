2f52f8237e64a8af5d73e324c29f501a3212aeb6
Austral1a <lienglex@gmail.com>
2020-11-18 10:22:54 +0200

    simplified useExtactValues hook

--- src/FirstForm/hooks/useExtractValues.jsx
+++ src/FirstForm/hooks/useExtractValues.jsx
@@ -1,11 +1,11 @@
 import {useSelector} from 'react-redux'
 function useExtractValues() {
-    const values = {}
-    values.firstName = useSelector(state => state.saveValueReducer.firstName)
-    values.lastName = useSelector(state => state.saveValueReducer.lastName)
-    values.email = useSelector(state => state.saveValueReducer.email)
-    values.phone = useSelector(state => state.saveValueReducer.phone)
-    return values
+    return {
+        firstName: useSelector(state => state.saveValueReducer.firstName),
+        lastName: useSelector(state => state.saveValueReducer.lastName),
+        email: useSelector(state => state.saveValueReducer.email),
+        phone: useSelector(state => state.saveValueReducer.phone)
+    }
 }
 
 export default useExtractValues
