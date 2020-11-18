ccde50b12040b7678d101c5bac38c1c54acbeca1
Austral1a <lienglex@gmail.com>
2020-11-18 15:27:14 +0200

    test: masks

---
+++ src/helpers/masks/__tests__/masks.test.js
@@ -0,0 +1,17 @@
+import {phoneMask} from '../masks'
+
+import {shallow} from 'enzyme'
+import Input from '../../../Components/common/Input/Input'
+
+describe('Phone Mask', () => {
+    it("phone mask must executing", () => {
+        const phoneMask = jest.fn()
+
+        const wrapper = shallow(<Input onInput={phoneMask} />)
+
+        wrapper.find('input').simulate('input')
+        wrapper.find('input').simulate('input')
+        wrapper.find('input').simulate('input')
+        expect(phoneMask).toHaveBeenCalledTimes(3)
+    })
+})
