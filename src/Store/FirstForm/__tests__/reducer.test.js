848224d71d477cc39dcb08ed457f3351ab995d7f
Austral1a <lienglex@gmail.com>
2020-11-19 11:07:12 +0200

    upd: reducer

--- src/Store/FirstForm/__tests__/reducer.test.js
+++ src/Store/FirstForm/__tests__/reducer.test.js
@@ -1,28 +1,33 @@
-import saveValueReducer from '../reducer'
-import * as types from '../action-types'
+import {saveValueReducer} from '../reducer'
+import {saveValues} from '../action'
 
 describe('save values reducer', () => {
-    const initState = {
-        firstName: '',
-        lastName: '',
-        email: '',
-        phone: ''
-    }
-    const values = {
-        firstName: 'Kyle',
-        lastName: 'Butler',
-        email: 'email@e.com',
-        phone: '(123) 456 - 7890'
-    }
+    const firstName = 'Kyle'
+    const lastName = 'Butler'
+    const email = 'email@e.com'
+    const phone = '(123) 456 - 7890'
+
     it('must return initial state', () => {
+        const initState = {
+            firstName: '',
+            lastName: '',
+            email: '',
+            phone: ''
+        }
+
         expect(saveValueReducer(undefined, {})).toStrictEqual(initState)
     })
 
     it('must handle SAVE_VALUES_IN_STORE', () => {
-        expect(saveValueReducer({}, {
-            type: types.SAVE_VALUES_IN_STORE,
-            ...values
-        })).toStrictEqual(values)
+        const expectedObj = {
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+            phone: phone
+        }
+        const args = [firstName, lastName, email, phone]
+
+        expect(saveValueReducer({}, saveValues(...args))).toStrictEqual(expectedObj)
     })
 
 })
