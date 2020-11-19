3de1b9cc174b7ee1b49551274ccb6e7f2654160d
Austral1a <lienglex@gmail.com>
2020-11-19 11:38:16 +0200

    changed pattern names by constants

--- src/FirstForm/hooks/useValidations.jsx
+++ src/FirstForm/hooks/useValidations.jsx
@@ -1,11 +1,13 @@
 import {useInputValidation} from '../index'
+import {patternNames} from '../../helpers'
 
-export function useValidations(firstName, lastName, email, phone) {
+export function useValidations(firstName, lastName, emailVal, phoneVal) {
+    const {name, email, phone} = patternNames
+
     return {
-        isFirstNameValid: useInputValidation('name', firstName),
-        isLastNameValid: useInputValidation('name', lastName),
-        isEmailValid: useInputValidation('email', email),
-        isPhoneValid: useInputValidation('phone', phone)
+        isFirstNameValid: useInputValidation(name, firstName),
+        isLastNameValid: useInputValidation(name, lastName),
+        isEmailValid: useInputValidation(email, emailVal),
+        isPhoneValid: useInputValidation(phone, phoneVal)
     }
 }
-
