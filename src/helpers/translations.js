1cf082bb2f7a608218daba2c80a8207361edb406
Austral1a <lienglex@gmail.com>
2020-11-19 10:18:19 +0200

    add: inputs translation; extracted validationErrors into translations

--- src/helpers/translations.js
+++ src/helpers/translations.js
@@ -14,5 +14,23 @@ export const translations = {
             basicVal: 'Basic',
             premiumVal: 'Premium'
         }
+    },
+    inputs: {
+        firstNameT: {
+            placeholder: 'First Name',
+            errMessage: 'First name must contain only letters and not to be empty',
+        },
+        lastNameT: {
+            placeholder: 'Last Name',
+            errMessage: 'Last name must contain only letters and not to be empty'
+        },
+        emailT: {
+            placeholder: 'Email',
+            errMessage: "Email must be appropriate to 'email@example.com'"
+        },
+        phoneT: {
+            placeholder: 'Phone',
+            errMessage: "Phone must be appropriate to '(123) 456 - 7890'"
+        }
     }
 }
--- src/helpers/validationMessages.js
+++
@@ -1,6 +0,0 @@
-export const validationMessages = {
-    firstName: 'First name must contain only letters and not to be empty',
-    lastName: 'Last name must contain only letters and not to be empty',
-    email: "Email must be appropriate to 'email@example.com'",
-    phone: "Phone must be appropriate to '(123) 456 - 7890'"
-};
