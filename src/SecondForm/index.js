f4cb467b7414f0b627e41629d2387729b396a74e
Austral1a <lienglex@gmail.com>
2020-11-19 09:36:30 +0200

    moved variables partial into styles folder

--- src/SecondForm/index.js
+++ src/SecondForm/index.js
@@ -1,36 +1,3 @@
-/*
-import Input from '../Components/Input/Input'
-import Select from '../Components/Select/Select'
-import Option from '../Components/Select/Option/Option'
-import Form from '../Components/Form/Form'
-import Card from '../Components/Card/Card'
-import subscriptions from '../helpers/subscriptions'
-import getPrice from '../helpers/getPrice/getPrice'
-import {countryChange} from '../Store/SecondForm/actions'
-import {planChange} from '../Store/SecondForm/actions'
-import Button from '../Components/Button/Button'
-import ErrorBoundary from '../ErrorBoundary'
-import useExtractValues from './hooks/useExtractValues'
-import {routes} from '../routes'
-import {Link} from 'react-router-dom'
-
-export {
-    Input,
-    Select,
-    Option,
-    Form,
-    Card,
-    subscriptions,
-    getPrice,
-    countryChange,
-    planChange,
-    Button,
-    ErrorBoundary,
-    useExtractValues,
-    routes,
-    Link}
-*/
-
 export * from './SecondForm'
 export * from './hooks'
 export * from './SecondForm.scss'
