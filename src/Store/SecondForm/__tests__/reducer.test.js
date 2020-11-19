b4ce7e79d0dbf04760fe5852c39dfd27f37c7ccb
Austral1a <lienglex@gmail.com>
2020-11-19 11:13:45 +0200

    upd: reducer test

--- src/Store/SecondForm/__tests__/reducer.test.js
+++ src/Store/SecondForm/__tests__/reducer.test.js
@@ -1,4 +1,4 @@
-import * as types from '../action-types'
+import {countryChange, planChange} from '../actions'
 import secondFormReducer from '../reducer'
 
 describe('Second Form reducer', () => {
@@ -21,28 +21,17 @@ describe('Second Form reducer', () => {
     })
 
     it('must handle COUNTRY_SELECT_CHANGE', () => {
-        expect(secondFormReducer({}, {
-            type: types.COUNTRY_SELECT_CHANGE,
-            ...newCountry
-        })).toStrictEqual(newCountry)
+        expect(secondFormReducer({}, countryChange(newCountry.country))).toStrictEqual(newCountry)
     })
 
     it('must handle PLAN_SELECT_CHANGE', () => {
-        expect(secondFormReducer({}, {
-            type: types.PLAN_SELECT_CHANGE,
-            ...newPlan
-        })).toStrictEqual(newPlan)
+        expect(secondFormReducer({}, planChange(newPlan.plan))).toStrictEqual(newPlan)
     })
 
     it('must handle whole actions', () => {
         const returnedState = {
-            ...secondFormReducer({}, {
-                type: types.COUNTRY_SELECT_CHANGE,
-                country: newState.country}),
-            ...secondFormReducer({}, {
-                type: types.PLAN_SELECT_CHANGE,
-                plan: newState.plan
-            })
+            ...secondFormReducer({}, countryChange(newCountry.country)),
+            ...secondFormReducer({}, planChange(newPlan.plan))
         }
         expect(returnedState).toStrictEqual(newState)
     })
